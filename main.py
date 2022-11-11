39cbfd329f7ff2c2874ea5ba1a7af38cddc445d2
Daniel <danielmontrest@gmail.com>
2022-11-11 13:15:29 -0500

    Commit realizado desde VS Code

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/hola')
-def hello():
+
+@app.route('/')
+def index():
     user_ip = request.remote_addr
-    return 'Hola, tu IP es {}'.format(user_ip)
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+
+    return render_template('hello.html', user_ip=user_ip)
