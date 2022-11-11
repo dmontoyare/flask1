0078a9c92dbf5170cede62af7eb0d3ca5eb3870f
Daniel <danielmontrest@gmail.com>
2022-11-11 11:21:26 -0500

    Se adiciona obtencion de ip del usuario

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/hola')
 def hello():
-    return 'Hello World Flask'
+    user_ip = request.remote_addr
+    return 'Hola, tu IP es {}'.format(user_ip)
